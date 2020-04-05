4987e45c01c1e061f3f4d150ba203aad868cccca
Cédric Viaccoz <cedric.viaccoz@gmail.com>
2020-04-05 22:06:37 +0100

    Refactorized for packaging

---
+++ build/lib/wavescape/__init__.py
@@ -0,0 +1,1 @@
+from wavescape.wavescape import Wavescape, produce_pitch_class_matrix_from_filename, apply_dft_to_pitch_class_matrix, complex_utm_to_ws_utm
---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+# source : https://packaging.python.org/tutorials/packaging-projects/
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wavescape-viaccoz",
+    version="0.0.1",
+    author="Cédric Viaccoz",
+    author_email="cedric.viaccoz@gmail.com",
+    description="Python library to build wavescapes, plot used in musicology",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dcml/wavescapes",
+    packages=setuptools.find_packages(),
+    install_requires = [
+        'numpy',
+        'music21',
+        'pretty_midi',
+        'drawSvg',
+        'librosa',
+        'scipy'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
